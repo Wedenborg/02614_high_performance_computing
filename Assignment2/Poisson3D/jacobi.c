87492f642418c6f99ec9a4080987a5269d0d7b57
Mikkel Sinkjaer <mikkelsinkjaer@mikkels-mbp-2.eduroam.wireless.dtu.dk>
2020-01-14 16:56:35 +0100

    Update jacobi.c

--- Assignment2/Poisson3D/jacobi.c
+++ Assignment2/Poisson3D/jacobi.c
@@ -3,7 +3,18 @@
  */
 #include <math.h>
 
-
+int f(int i,int j,int k){
+    if( i >= -1 && i <= 0.375 ){
+        if( j >= -1 && j <= -0.5){
+            if( k >= -0.66666666667  && k <= 0){
+                return 200;
+            }
+        }
+    }
+    else{
+        return 0;
+    }
+}
 
 void jacobi(int N, double ***u, int max_iter) {
 
@@ -29,8 +40,8 @@ void jacobi(int N, double ***u, int max_iter) {
         for( int i =0; i < N; i++){
             for( int j = 0; j < N; j++){
                 for( int k = 0; k < N; k++){
-	                u[i][j][k]= 1/6*(v[i-1][j][k]+v[i+1][j][k]+v[i][j-1][k]+v[i][j+1][k]+v[i][j][k-1]+v[i][j][k+1]); //formula and matrix
-	                stopTest +=(u[i][j][k]-v[i][j][k])*(u[i][j][k]-v[i][j][k]);
+	                u[i][j][k]= 1/6*(v[i-1][j][k]+v[i+1][j][k]+v[i][j-1][k]+v[i][j+1][k]+v[i][j][k-1]+v[i][j][k+1] + N*N * f(i,j,k)); //formula and matrix      
+                    stopTest +=(u[i][j][k]-v[i][j][k])*(u[i][j][k]-v[i][j][k]);
 
 	                  }
 
