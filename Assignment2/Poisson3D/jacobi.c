0f5f956c9629ce80ef54a50510df5b37a5c0d779
Mikkel Sinkjaer <mikkelsinkjaer@mikkels-mbp-2.eduroam.wireless.dtu.dk>
2020-01-14 16:42:20 +0100

    Update jacobi.c

--- Assignment2/Poisson3D/jacobi.c
+++ Assignment2/Poisson3D/jacobi.c
@@ -2,26 +2,33 @@
  * 
  */
 #include <math.h>
-jacobi(int N, double ***u) {
+jacobi(int N, double ***u, int max_iter) {
 
-    double ***u_old = NULL;
     double ***u_new = NULL;
     double cond =0.005;
     double stopTest = 100000;
-    double *** v = 0;
+    double *** v = v;
 
     if ( (uu = d_malloc_3d(N, N, N)) == NULL ) {
         perror("array u: allocation failed");
         exit(-1);
 
 
+
+    while( sqrt(stopTest)<max_iter){
+
     for( int i =0; i < N; i++){
         for( int j = 0; j < N; j++){
             for( int k = 0; k < N; k++){
-              	  while( sqrt(stopTest)<cond){
+                v[i][j][k] = u[i][j][k];
+            }
+        }
+    }
+        for( int i =0; i < N; i++){
+            for( int j = 0; j < N; j++){
+                for( int k = 0; k < N; k++){
 	                  stopTest +=(u_new[i][j][k]-u_old[i][j][k])*(u_new[i][j][k]-u_old[i][j][k]);
-	                  u_old[i][j][k] = u_new[i][j][k];
-	                  v = 1/6*(u[i-1][j][k]+u[i+1][j][k]+u[i][j-1][k]+u[i][j+1][k]+u[i][j][k-1]+u[i][j][k+1]); //formula and matrix
+	                   u[i][j][k]= 1/6*(v[i-1][j][k]+v[i+1][j][k]+v[i][j-1][k]+v[i][j+1][k]+v[i][j][k-1]+v[i][j][k+1]); //formula and matrix
 
 
 	                  }
