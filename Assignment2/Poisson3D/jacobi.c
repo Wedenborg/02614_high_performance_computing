a91ecd5e0722a0da76a957de0e8983161c5826ac
Mikkel Sinkjaer <mikkelsinkjaer@mikkels-mbp-2.eduroam.wireless.dtu.dk>
2020-01-15 15:03:55 +0100

    jacobi!!!!!!!!!!!!!!!!!!!Q!!!!

--- Assignment2/Poisson3D/jacobi.c
+++ Assignment2/Poisson3D/jacobi.c
@@ -21,7 +21,7 @@ void jacobi(int N, double ***u, double ***v, double ***f, int iter_max, double t
         for( int i =1; i < N-1; i++){
             for( int j = 1; j < N-1; j++){
                 for( int k = 1; k < N-1; k++){
-	                u[i][j][k] = 1./6.*(v[i-1][j][k]+v[i+1][j][k]+v[i][j-1][k]+v[i][j+1][k]+v[i][j][k-1]+v[i][j][k+1] + (N-2)*(N-2) * f[i][j][k]); //formula and matrix      
+	                u[i][j][k] = 1./6.*(v[i-1][j][k]+v[i+1][j][k]+v[i][j-1][k]+v[i][j+1][k]+v[i][j][k-1]+v[i][j][k+1] + 1./((N-2)*(N-2)) * f[i][j][k]); //formula and matrix      
                     //printf("u(%d,%d,%d) = %lf \n",i,j,k, u[i][j][k]);
 
                     //printf("v(%d,%d,%d) = %lf \n",i,j,k, v[i][j][k]);
