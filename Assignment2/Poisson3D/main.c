a52a8e98ea129176fa7c52ed5f98060cae5b510d
Wedenborg <emiliewedenborg@gmail.com>
2020-01-15 12:01:45 +0100

    Update main.c

--- Assignment2/Poisson3D/main.c
+++ Assignment2/Poisson3D/main.c
@@ -29,7 +29,7 @@ main(int argc, char *argv[]) {
     char	output_filename[FILENAME_MAX];
     double 	***u = NULL;
     double *** v = NULL;
-    double *** f = NULL
+    double *** f = NULL;
 
 
     /* get the paramters from the command line */
