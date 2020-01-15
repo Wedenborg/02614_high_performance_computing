fadd1352ccdc24051c6b6d7e8e0b0c3400f1649f
Wedenborg <emiliewedenborg@gmail.com>
2020-01-15 11:40:34 +0100

    Update jacobi.c

--- Assignment2/Poisson3D/jacobi.c
+++ Assignment2/Poisson3D/jacobi.c
@@ -32,8 +32,8 @@ void jacobi(int N, double ***u, double ***v, int iter_max) {
         }
     }
         for( int i =1; i < N-1; i++){
-            for( int j = 0; j < N; j++){
-                for( int k = 0; k < N; k++){
+            for( int j = 1; j < N-1; j++){
+                for( int k = 1; k < N-1; k++){
 	                u[i][j][k]= 1/6*(v[i-1][j][k]+v[i+1][j][k]+v[i][j-1][k]+v[i][j+1][k]+v[i][j][k-1]+v[i][j][k+1] + N*N * f(i,j,k)); //formula and matrix      
                     stopTest +=(u[i][j][k]-v[i][j][k])*(u[i][j][k]-v[i][j][k]);
 	                  }
