24686640a7c634b21b9d01e5b5a9e4b426bd070d
Mikkel Sinkjaer <mikkelsinkjaer@mikkels-mbp-2.eduroam.wireless.dtu.dk>
2020-01-16 14:21:08 +0100

    jacobi par

--- Assignment2/Poisson3D/jacobi_v2.c
+++ Assignment2/Poisson3D/jacobi_v2.c
@@ -4,11 +4,14 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 
-void gauss_seidel(int N, double ***u, double ***v, double ***f, int iter_max, double tolerance) {
+void jacobi(int N, double ***u, double ***v, double ***f, int iter_max, double tolerance) {
     double stopTest = 100000;
-    int counter =0 ;
-
+    int counter =0;
+    int i,j,k;
     while( sqrt(stopTest)>tolerance && counter < iter_max){
         stopTest =0.0;
         for( int i =0; i < N; i++){
@@ -18,10 +21,11 @@ void gauss_seidel(int N, double ***u, double ***v, double ***f, int iter_max, do
                 }
             }
         }
-        for( int i =1; i < N-1; i++){
-            for( int j = 1; j < N-1; j++){
-                for( int k = 1; k < N-1; k++){
-	                u[i][j][k] = 1./6.*(u[i-1][j][k]+u[i+1][j][k]+u[i][j-1][k]+u[i][j+1][k]+u[i][j][k-1]+u[i][j][k+1] + 1./((N-2)*(N-2)) * f[i][j][k]); //formula and matrix      
+        #pragma omp parallel for shared(u, v, N) private(i, j, k) reduction(+: stopTest)
+        for( i =1; i < N-1; i++){
+            for( j = 1; j < N-1; j++){
+                for( k = 1; k < N-1; k++){
+	                u[i][j][k] = 1./6.*(v[i-1][j][k]+v[i+1][j][k]+v[i][j-1][k]+v[i][j+1][k]+v[i][j][k-1]+v[i][j][k+1] + 1./((N-2)*(N-2)) * f[i][j][k]); //formula and matrix      
                     //printf("u(%d,%d,%d) = %lf \n",i,j,k, u[i][j][k]);
 
                     //printf("v(%d,%d,%d) = %lf \n",i,j,k, v[i][j][k]);
@@ -30,13 +34,17 @@ void gauss_seidel(int N, double ***u, double ***v, double ***f, int iter_max, do
                     
 	            }
             }
-            
-        }
-        //printf("stopTest: %lf \n",stopTest);
-        //printf("count: %d \n",counter); 
+            /*
+            int t_id = -1;
+
+            t_id = omp_get_thread_num(); 
+
+            printf("Worker:  %d!\n", t_id);
+            */
+        } // End omp
         counter++;
+    
     }
     free(v);
 }
-
 
