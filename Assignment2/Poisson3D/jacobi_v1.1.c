67cbaf40e6134c6ca14324b4ee18f897a05a7ef8
Mikkel Sinkjaer <mikkelsinkjaer@mikkels-mbp-2.eduroam.wireless.dtu.dk>
2020-01-16 17:27:44 +0100

    adfs

--- Assignment2/Poisson3D/jacobi_v1.1.c
+++ Assignment2/Poisson3D/jacobi_v1.1.c
@@ -4,10 +4,14 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 
 void jacobi(int N, double ***u, double ***v, double ***f, int iter_max, double tolerance) {
     double stopTest = 100000;
-    int counter =0 ;
+    int counter =0;
+    int i,j,k;
 
     while( sqrt(stopTest)>tolerance && counter < iter_max){
         stopTest =0.0;
@@ -18,23 +22,29 @@ void jacobi(int N, double ***u, double ***v, double ***f, int iter_max, double t
                 }
             }
         }
-        for( int i =1; i < N-1; i++){
-            for( int j = 1; j < N-1; j++){
-                for( int k = 1; k < N-1; k++){
+        #pragma omp parallel for shared(u, v, N) private(i, j, k) reduction(+: stopTest)
+        for( i =1; i < N-1; i++){
+            for( j = 1; j < N-1; j++){
+                for( k = 1; k < N-1; k++){
 	                u[i][j][k] = 1./6.*(v[i-1][j][k]+v[i+1][j][k]+v[i][j-1][k]+v[i][j+1][k]+v[i][j][k-1]+v[i][j][k+1] + 1./((N-2)*(N-2)) * f[i][j][k]); //formula and matrix      
                     //printf("u(%d,%d,%d) = %lf \n",i,j,k, u[i][j][k]);
 
                     //printf("v(%d,%d,%d) = %lf \n",i,j,k, v[i][j][k]);
 
-                    stopTest +=sqrt((u[i][j][k]-v[i][j][k])*(u[i][j][k]-v[i][j][k]));
+                    stopTest +=(u[i][j][k]-v[i][j][k])*(u[i][j][k]-v[i][j][k]);
                     
 	            }
             }
-            
-        }
-        //printf("stopTest: %lf \n",stopTest);
-        //printf("count: %d \n",counter); 
+            /*
+            int t_id = -1;
+
+            t_id = omp_get_thread_num(); 
+
+            printf("Worker:  %d!\n", t_id);
+            */
+        } // End omp
         counter++;
+    
     }
     free(v);
 }
