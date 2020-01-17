28c9e1ec1b0636b9fb7a6280c5327acea4cd8d99
Wedenborg <emiliewedenborg@gmail.com>
2020-01-17 17:56:35 +0100

    Det eller sidste

--- Assignment2/Poisson3D/gauss_seidel.c
+++ Assignment2/Poisson3D/gauss_seidel.c
@@ -9,28 +9,26 @@ void gauss_seidel(int N, double ***u, double ***v, double ***f, int iter_max, do
     double stopTest = 100000;
     int counter =0 ;
 
+
     while(sqrt(stopTest)>tolerance && counter < iter_max){
         stopTest =0.0;
         for( int i =1; i < N-1; i++){
             for( int j = 1; j < N-1; j++){
                 for( int k = 1; k < N-1; k++){
+                    double temp =u[i][j][k];
 	                u[i][j][k] = 1./6.*(u[i-1][j][k]+u[i+1][j][k]+u[i][j-1][k]+u[i][j+1][k]+u[i][j][k-1]+u[i][j][k+1] + 1./((N-2)*(N-2)) * f[i][j][k]); //formula and matrix      
-                    //printf("u(%d,%d,%d) = %lf \n",i,j,k, u[i][j][k]);
 
-                    //printf("v(%d,%d,%d) = %lf \n",i,j,k, v[i][j][k]);
-
-                    stopTest +=(u[i][j][k]-v[i][j][k])*(u[i][j][k]-v[i][j][k]);
+                    stopTest +=(u[i][j][k]-temp)*(u[i][j][k]-temp);
                     
 	            }
-            }
-            
+            }    
         }
-	//printf("%d ",counter);
-        //printf("%lf \n ",stopTest);
+	    printf("%d ",counter);
+        printf("%lf \n ",stopTest);
          
         counter++;
     }
-    printf("Total iterations: %d ",counter);
+    //printf("Total iterations: %d ",counter);
 }
 
 
