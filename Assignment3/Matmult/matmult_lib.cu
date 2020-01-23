82085a141f1b482aa1abf030bc62e1fb72b5c35a
Mikkel Sinkjaer <mikkelsinkjaer@mikkels-mbp-2.eduroam.wireless.dtu.dk>
2020-01-23 19:30:33 +0100

    gpulib

--- Assignment3/Matmult/matmult_lib.cu
+++ Assignment3/Matmult/matmult_lib.cu
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "cublas_v2.h"
 extern "C"{
 #ifdef _OPENMP
 #include <omp.h>
@@ -7,6 +8,7 @@ extern "C"{
 #include "cblas.h"
 
 #define size 8
+#define IDX2C(i,j,ld) (((j)*(ld))+(i))
 
 __global__ void matcal_1(int m,int n,int k, double *A,double *B, double *C){
 
@@ -28,8 +30,8 @@ __global__ void matcal_1(int m,int n,int k, double *A,double *B, double *C){
 __global__ void matcal_2(int m,int n,int k, double *A,double *B, double *C){
 
     // 2D thread indices defining row and col of element
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    int i = blockIdx.x * blockDim.x + threadIdx.x; // Row index in C
+    int j = blockIdx.y * blockDim.y + threadIdx.y; // Col index in C
 
     if ( i<m && j<n ){
 
@@ -42,30 +44,30 @@ __global__ void matcal_2(int m,int n,int k, double *A,double *B, double *C){
 };
 
 // below version
-// __global__ void matcal_3(int m,int n,int k, double *A,double *B, double *C){
+    // __global__ void matcal_3(int m,int n,int k, double *A,double *B, double *C){
 
-//     // 2D thread indices defining row and col of element
-//     int i = 2*(blockIdx.x * blockDim.x + threadIdx.x);
-//     int j = blockIdx.y * blockDim.y + threadIdx.y;
+    //     // 2D thread indices defining row and col of element
+    //     int i = 2*(blockIdx.x * blockDim.x + threadIdx.x);
+    //     int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-//     double c1 = 0;
-//     double c2 = 0;
+    //     double c1 = 0;
+    //     double c2 = 0;
 
-//         if ( i<m && j<n  && (i+1)<m ){
-//             for (int h= 0;h<k;h++){
-//                 c1 +=  A[i*k + h]*B[h*n + j];
+    //         if ( i<m && j<n  && (i+1)<m ){
+    //             for (int h= 0;h<k;h++){
+    //                 c1 +=  A[i*k + h]*B[h*n + j];
 
-//                 c2 +=  A[(i+1)*k + h]*B[h*n + j];
-//             } 
-//             C[i*n + j] =c1;
-//             C[(i+1)*n + j]=c2;
-//         }
-//         else if( i<m && j<n){
-//             for (int h= 0;h<k;h++){
-//                 c1+=  A[i*k + h]*B[h*n + j];
-//             }
-//             C[i*n + j] =c1;
-//         }
+    //                 c2 +=  A[(i+1)*k + h]*B[h*n + j];
+    //             } 
+    //             C[i*n + j] =c1;
+    //             C[(i+1)*n + j]=c2;
+    //         }
+    //         else if( i<m && j<n){
+    //             for (int h= 0;h<k;h++){
+    //                 c1+=  A[i*k + h]*B[h*n + j];
+    //             }
+    //             C[i*n + j] =c1;
+    //         }
 
     
 // };
@@ -285,8 +287,6 @@ __global__ void matcal_4(int m,int n,int k, double *A,double *B, double *C){
 
 void matmult_lib (int m, int n, int k, double *A, double *B, double *C){
 
-
-
     double alpha = 1.0;
     int beta = 0;
     double ts, te;
@@ -298,6 +298,60 @@ void matmult_lib (int m, int n, int k, double *A, double *B, double *C){
     cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans, m,n, k ,alpha, A, lda, B, ldb, beta, C, ldc);
     te = omp_get_wtime() - ts;
     printf("%lf \n",te);
+}
+
+void matmult_gpulib (int m, int n, int k, double *h_A, double *h_B, double *h_C){
+
+    
+    const double alf = 1;
+    const double bet = 0;
+    const double *alpha = &alf;
+    const double *beta = &bet;
+    
+    double ts, te;
+    int lda = k;
+    int ldb = n;
+    int ldc = n;
+    double *d_A;
+    double *d_B;
+    double *d_C;
+
+
+    // cublasDgemm(cublasHandle_t handle,
+    //     cublasOperation_t transa, cublasOperation_t transb,
+    //     int m, int n, int k,
+    //     const double          *alpha,
+    //     const double          *A, int lda,
+    //     const double          *B, int ldb,
+    //     const double          *beta,
+    //     double          *C, int ldc)
+
+    // Allocating memory on the device  
+    cudaMalloc((void**)&d_A, m*k * sizeof(double));
+    cudaMalloc((void**)&d_B, n*k * sizeof(double));
+    cudaMalloc((void**)&d_C, m*n * sizeof(double));
+
+    // Copying input matrix A and B to the device 
+    cudaMemcpy(d_A, h_A,  m*k * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B,  n*k * sizeof(double), cudaMemcpyHostToDevice);
+    
+    // Create a handle for CUBLAS
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+    
+    // ts = omp_get_wtime();
+    cublasDgemm(handle,CUBLAS_OP_N,CUBLAS_OP_N, m,n, k ,alpha, d_A, lda, d_B, ldb, beta, d_C, ldc);
+    // te = omp_get_wtime() - ts;
+    // printf("%lf \n",te);    // Copying the calculated C matrix to host 
+    
+    // Destroy the handle
+    cublasDestroy(handle);
+    cudaMemcpy(h_C, d_C, m*n * sizeof(double), cudaMemcpyDeviceToHost);
+
+    // Free all allocated memory on the hos 
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
 }
 
 void matmult_gpu1(int m, int n, int k, double *h_A,double *h_B,double *h_C){
@@ -356,32 +410,32 @@ void matmult_gpu2(int m, int n, int k, double *h_A,double *h_B,double *h_C){
 }
 
 // Below version
-// void matmult_gpu3(int m, int n, int k, double *h_A,double *h_B,double *h_C){
+    // void matmult_gpu3(int m, int n, int k, double *h_A,double *h_B,double *h_C){
 
-//     double *d_A;
-//     double *d_B;
-//     double *d_C;
+    //     double *d_A;
+    //     double *d_B;
+    //     double *d_C;
 
 
-//     cudaMalloc((void**)&d_A, m*k * sizeof(double));
-//     cudaMalloc((void**)&d_B, n*k * sizeof(double));
-//     cudaMalloc((void**)&d_C, m*n * sizeof(double));
+    //     cudaMalloc((void**)&d_A, m*k * sizeof(double));
+    //     cudaMalloc((void**)&d_B, n*k * sizeof(double));
+    //     cudaMalloc((void**)&d_C, m*n * sizeof(double));
 
-//     cudaMemcpy(d_A, h_A,  m*k * sizeof(double), cudaMemcpyHostToDevice);
-//     cudaMemcpy(d_B, h_B,  n*k * sizeof(double), cudaMemcpyHostToDevice);
+    //     cudaMemcpy(d_A, h_A,  m*k * sizeof(double), cudaMemcpyHostToDevice);
+    //     cudaMemcpy(d_B, h_B,  n*k * sizeof(double), cudaMemcpyHostToDevice);
 
-//     dim3 dimBlock(32,32,1); // threads per block
-//     dim3 dimGrid((m/(32*2)+ 1),n/32 + 1,1);// blocks in total
+    //     dim3 dimBlock(32,32,1); // threads per block
+    //     dim3 dimGrid((m/(32*2)+ 1),n/32 + 1,1);// blocks in total
 
-//     matcal_3<<<dimGrid, dimBlock>>>(m,n,k,d_A,d_B,d_C);
-//     cudaDeviceSynchronize();
+    //     matcal_3<<<dimGrid, dimBlock>>>(m,n,k,d_A,d_B,d_C);
+    //     cudaDeviceSynchronize();
 
-//     cudaMemcpy(h_C, d_C, m*n * sizeof(double), cudaMemcpyDeviceToHost);
+    //     cudaMemcpy(h_C, d_C, m*n * sizeof(double), cudaMemcpyDeviceToHost);
 
-//     cudaFree(d_A);
-//     cudaFree(d_B);
-//     cudaFree(d_C);
-// }
+    //     cudaFree(d_A);
+    //     cudaFree(d_B);
+    //     cudaFree(d_C);
+    // }
 
 // right version
 void matmult_gpu3(int m, int n, int k, double *h_A,double *h_B,double *h_C){
@@ -437,6 +491,5 @@ void matmult_gpu4(int m, int n, int k, double *h_A,double *h_B,double *h_C){
     cudaFree(d_B);
     cudaFree(d_C);
 }
-
 
 }
